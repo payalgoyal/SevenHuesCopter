6925fcf3aa15dac8384a64d2a3b37b6cd2cf7e48
payalgoyal <payalgoyal90@gmail.com>
2015-10-04 01:13:29 +0530

    Edited bg and obstacle images

--- main.js
+++ main.js
@@ -8,51 +8,42 @@ var pipesTime = 2000;
 var score;
 var countLeft = 0;
 var verticalSprite;
-//var blaster;
 
 var main = function(game){}
 // Creates a new 'main' state that will contain the game
    main.prototype = {
 		// Function called first to load all the assets
 		preload: function() { 
-			// Change the background color of the game
-			game.stage.backgroundColor = '#000';
-			
-			//game.load.image("background", "assets/raining.jpg");
-			
-			// Load the player sprite
-			game.load.image('player', 'assets/player.png');  
-
-			// Load the pipe sprite
-			game.load.image('pipe', 'assets/pipe.png');   
-			
-			// Load the vertical obstacle spritesheet
-			game.load.spritesheet('verticalObstacle', 'assets/verticalObstacle.png',24,145,9);   
-			
-			// Load the vertical obstacle gif
-			game.load.image('black', 'assets/black.png');   
-			
-			// // Load dynamite image
-			// game.load.image('dynamite', 'assets/dynamite.jpg'); 
-
-			// Load the extraPoints image
-			game.load.image('extraPoints', 'assets/extraPoints.png');   
-			
-			// Load explosion image
-			game.load.image('explosion', 'assets/explosion.jpg');  
-			
-			// //Load audio
-			// game.load.audio('blaster', 'assets/audio/blaster.mp3');
+			// Change the background color of the game			
+			game.load.image("layer1", "assets/layer-1_small.png");
+			game.load.image("layer3", "assets/layer-3_small.png");
+			game.load.image("layer2", "assets/layer-2_small.png");
+			game.load.image("layer6", "assets/layer-6_small.png");
+			game.load.image("extraPoints", "assets/extraPoints.png");
+			game.load.image("player", "assets/plane.png");
+			game.load.image("pipe", "assets/obstacle.png");
 		},
 
 		// Fuction called after 'preload' to setup the game 
-		create: function() { 	
-			// dynamite = game.add.group();
-			// dynamite.enableBody = true;
-			// dynamite.createMultiple(4, 'dynamite'); 
+		create: function() { 		
+			// // // layers = game.add.group();
+			layer1 = game.add.sprite(0, 0, 'layer1');
+			layer1_dup = game.add.sprite(900, 0, 'layer1');
+			// // // layers.add(layer1);
+			// // // // layer1.z = 0;
 			
-			//game.add.tileSprite(0, 0, 1000, 600, 'background');
-		
+			layer3 = game.add.sprite(0, 0, 'layer3');
+			layer3_dup = game.add.sprite(1000, 0, 'layer3');
+			
+			layer2 = game.add.sprite(0, 0, 'layer2');
+			layer2_dup = game.add.sprite(980, 0, 'layer2');
+			// // // layers.add(layer2);
+			
+			// // // layers.add(layer3);
+			
+			layer6 = game.add.sprite(0, 0, 'layer6');
+			layer6_dup = game.add.sprite(1030, 0, 'layer6');
+			
 			// Create a group of 60 pipes
 			pipes = game.add.group();
 			pipes.enableBody = true;
@@ -84,12 +75,12 @@ var main = function(game){}
 			game.input.onDown.add(jump, this);
 
 			// Timer that calls 'addRowOfPipes' ever 2 seconds 
-			timer = game.time.events.loop(pipesTime, addObstacles, this);  
+			timer = game.time.events.loop(pipesTime, addRowOfPipes, this);  
 			
-			timer = game.time.events.loop(500, moveVerticalPipes, this); 
-		
+			//timer = game.time.events.loop(500, moveVerticalObstacle, this); 
+			
 			// addVerticalPipes();
-			// timer = game.time.events.loop(2000, addVerticalPipes, this);
+			timer = game.time.events.loop(3400, addVerticalObstacle, this);
 			
 			score = 0;
 			// if (score <= 10){
@@ -110,18 +101,19 @@ var main = function(game){}
 			
 			//player.animations.add("explode",'player',30,true);
 			updateScore();
-			
+
 		},
 
 		// This function is called 60 times per second
 		update: function() {
+			moveBackground(layer2,layer3,layer6);
+			moveBackground(layer2_dup,layer3_dup,layer6_dup);
+			//moveBackground(layer6);
+			// moveBackground(layer6_dup);
+			
 			// If the player is out of the world (too high or too low), call the 'restartGame' function
 			if (player.inWorld == false){
 				gameOver(); 
-			}
-			
-			if (score > 10){
-				game.stage.backgroundColor = '#FF0';
 			}
 			
 			// If the player overlap any pipes, call 'gameOver'
@@ -130,33 +122,69 @@ var main = function(game){}
 			// If the player overlap any flying objects, call 'addScore'
 			game.physics.arcade.overlap(player, extraPoints, addScore, null, this);
 			
-			// If the player overlap vertically swing Obstacle, call 'gameOver'
-			game.physics.arcade.overlap(player, verticalSprites, gameOver, null, this);
-			
-			// // If the player overlap any dynamite, call 'gameOver'
-			// game.physics.arcade.overlap(player, dynamite, gameOver, null, this); 
-			
 			computeScore();
-			//this.restartButton.onInputDown.add(this.restartGame,this);
 		}
+   }
+   
+   function moveBackground(layer2,layer3,layer6){
+	   if (layer2.x < -980){
+			layer2.x = 980;	
+			layer2.x -= 3;
+		}
+		else{
+			layer2.x -= 3;
+		}
+		if (layer3.x < -1000){
+			layer3.x = 1000;	
+			layer3.x -= 2;
+		}
+		else{
+			layer3.x -= 2;
+		}
+		if (layer6.x < -980){
+			layer6.x = 980;	
+			layer6.x -= 1;
+		}
+		else{
+			layer6.x -= 1;
+		}
+			
    }
    
 	game.state.add("Main",main);
     game.state.start("Main");
 	
-	function addVerticalPipes(){
-		verticalSprite = game.add.sprite(889, 200, 'verticalObstacle');
-		game.physics.enable(verticalSprite,Phaser.Physics.ARCADE);
-		verticalSprite.animations.add('right', [0, 1, 2, 3,4,5,6,7,8,9], 10, true);
-		verticalSprite.giveScore = true;
-		verticalSprites.add(verticalSprite);
+	function addVerticalObstacle(){
+		var ran = Math.floor(Math.random()*2)+1;
+	   
+		// Get the first dead pipe of our group
+        pipe = pipes.getFirstDead();
+		if (ran === 1){
+		   pipe.reset(500,0);
+		   pipe.angle = -4;
+		   pipe.height = 90;
+		   pipe.width = 60
+		   var tween = game.add.tween(pipe).to({x: 50, y: 600}, 4000);
+			tween.start();
+	   }
+	   else{
+		    pipe.reset(500,500);
+			pipe.angle = -4;
+			 pipe.height = 90;
+		   pipe.width = 60
+		   var tween = game.add.tween(pipe).to({x: 50, y: -200}, 4000);
+			tween.start();
+	   }
+		pipe.giveScore = true;
+        // Kill the pipe when it's no longer visible 
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
 	}
 	
-	function moveVerticalPipes(){
-		if (verticalSprite != undefined){
-			verticalSprite.animations.play('right');
-		}
-	}
+	// function moveVerticalObstacle(){
+		// var tween = game.add.tween(extraPoints).to({ x: 500,y: 500}, 3000);
+		// tween.start();
+	// }
 	
 	function updateScore(){
 		scoreText.text = "Score: "+score+"\nBest: "+topScore;	
@@ -214,41 +242,18 @@ var main = function(game){}
 	
     // Add a pipe on the screen
    function addOnePipe(x, y) {
-        // Get the first dead pipe of our group
+	   // Get the first dead pipe of our group
         pipe = pipes.getFirstDead();
+		
+	   // Set the new position of the pipe
+		pipe.reset(x, y);
+		 pipe.height = 90;
+		  pipe.width = 60
 
-        // Set the new position of the pipe
-        pipe.reset(x, y);
+		// Add velocity to the pipe to make it move left
+		pipe.body.velocity.x = -200; 
+   
 
-        // Add velocity to the pipe to make it move left
-		if (score < 10){
-			pipe.body.velocity.x = -200; 
-			if (verticalSprite != undefined){
-				verticalSprite.body.velocity.x = -400;
-			}
-		}
-		
-		else if (score >= 10 && score < 20){
-			pipe.body.velocity.x = -400; 
-			if (verticalSprite != undefined){
-				verticalSprite.body.velocity.x = -500;
-			}
-		}
-		
-		else if (score >= 20 && score <30){
-			pipe.body.velocity.x = -500;
-			if (verticalSprite != undefined){
-				verticalSprite.body.velocity.x = -600;
-			}
-		}
-		
-		else if (score >= 30){
-			pipe.body.velocity.x = -600; 
-			if (verticalSprite != undefined){
-				verticalSprite.body.velocity.x = -700;
-			}
-		}
-               
         // Kill the pipe when it's no longer visible 
         pipe.checkWorldBounds = true;
         pipe.outOfBoundsKill = true;
@@ -259,13 +264,10 @@ var main = function(game){}
 		if (gameAlive == true){
 			var place = Math.floor(Math.random()*2)+1;
 	
-			var ran = Math.floor(Math.random()*3)+4;
 			 if (place === 1) 
-				for (var i = 0; i < ran; i++)
-					addOnePipe(1289, i*50);  
+				addOnePipe(889, 100);  
 			else{
-				for (var i = 0; i < ran; i++)
-					addOnePipe(1289, 450-(i*50));  
+				addOnePipe(889, 250);  
 			}
 			pipe.giveScore = true;
 		}
@@ -342,3 +344,4 @@ var main = function(game){}
 			// dynamite.outOfBoundsKill = true;
 			// }
 	}
+
