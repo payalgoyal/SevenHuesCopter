55768ef3ce736f9fa11755038fca8e03b1fb5026
payalgoyal <payalgoyal90@gmail.com>
2015-10-25 00:31:25 +0530

    reduced rev img appearance time

--- home.js
+++ home.js
@@ -168,7 +168,7 @@ var home = function(game){}
 			
 			timer = game.time.events.loop(3967, addFloorsOfBuilding, this);
 			
-			timer = game.time.events.loop(16000, addReverseObject, this);
+			timer = game.time.events.loop(8000, addReverseObject, this);
 			
 			score = 0;
 			functionCalled = 0;
