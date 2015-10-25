212eb7bd0af3f4a44e61ae159ccb203755f45ba4
payalgoyal <payalgoyal90@gmail.com>
2015-10-25 19:07:01 +0530

    test splash size

--- main.js
+++ main.js
@@ -13,8 +13,10 @@ var main = function(game){}
 		
 		},
 		create: function() { 
-		splash = game.add.sprite(innerWidth/2, innerHeight/2, 'splash');
-		splash.anchor.set(0.5,0.5);
+		splash = game.add.sprite(0, 0, 'splash');
+		splash.width = window.screen.width;
+		splash.height = window.screen.height;
+		//splash.anchor.set(0.5,0.5);
 		// splash.width = window.innerWidth;
 		
 		game.input.onDown.add(startMenuPage, this);
