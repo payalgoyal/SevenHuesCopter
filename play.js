15464e22664a9d1ab68efb49ec1980eaa8d15517
payalgoyal <payalgoyal90@gmail.com>
2015-11-05 15:04:32 +0530

    Missed bgMusic on restart

--- play.js
+++ play.js
@@ -1400,7 +1400,7 @@ var play = function(game){}
 	function restart() {
 		killObstacles();
 		//my_media.pause();
-		// playAudio("bgmusic");
+		playAudio("bgmusic");
 		level = 0;
 		gameAlive = true;
 		skip = 0;
